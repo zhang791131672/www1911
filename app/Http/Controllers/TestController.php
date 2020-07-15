49156b540787e6a6cb733c99b509ba8f27a2d9fd
zhangbowen <791131672@qq.com>
2020-07-15 11:03:28 +0800

    获取微信access_token

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,4 +10,12 @@ class TestController extends Controller
     public function index(){
         echo "hello world";
     }
+
+    public function getAccessToken(){
+        $appid='wx5efbe8932db24806';
+        $appsecret='fe8604bcaaca5d3c5fdde138be496435';
+        $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$appsecret;
+        $res=file_get_contents($url);
+        dd($res);
+    }
 }
